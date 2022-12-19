757eb34738ef2dd72c915719a6342e09ce43913c
Tuğberk Abdulkadiroğlu <tugberka@sabanciuniv.edu>
2022-12-19 17:55:11 +0300

    navigation bar changed completely, pages links repaired

--- navigation/functions/delete.php
+++ navigation/functions/delete.php
@@ -5,7 +5,7 @@
     $result = mysqli_query($db, $sql_statement);
 
     if($result == 1) {
-        echo 'The user with id ' . $eid . ' has been successfully deleted from the system.: ' . '<br>' . 'Deletion result: ' . $result; 
+        echo 'The employee with id ' . $eid . ' has been successfully deleted from the system.: ' . '<br>' . 'Deletion result: ' . $result; 
     } else {
         echo 'There is an error. ' . '<br>' . 'Deletion result: ' . $result; 
     }
--- navigation/pages/employee_insertion_view.php
+++ navigation/pages/employee_insertion_view.php
@@ -1,17 +1,19 @@
-<link rel="stylesheet" href="../../styles/nav_menu.css">
-<link rel="stylesheet" href="../../styles/body.css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="../../styles/nav_menu.css" />
+<link rel="stylesheet" href="../../styles/body.css" />
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet" />
 
-<nav class="navMenu">
-    <ul>
-        <a href="admin_view.php" class="active">Admin</a>
-        <a href="employee_insertion_view.php">Employee Insertion</a>
-        <a href="filter_view.php">Filter</a>
-        <a href="get_employees_view.php">Get All Employees</a>
-    </ul>
-</nav>
+<div class="navMenuContainer">
+    <nav class="navMenu">
+        <ul>
+            <a href="admin_view.php">Admin</a>
+            <a href="employee_insertion_view.php" class="active">Employee Insertion</a>
+            <a href="filter_view.php">Filter</a>
+            <a href="get_employees_view.php">Get All Employees</a>
+        </ul>
+    </nav>
+</div>
+
+
 
 <form action="../functions/employee_insertion.php", method="POST">
     Employee id:
